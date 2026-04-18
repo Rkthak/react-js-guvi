e455af6d44f41b05ce8e637d32f64d563ab87f8f
Rkthak <rkon2609@gmail.com>
2026-04-18 13:59:41 +0530

    functional components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,3 @@
-// class component ==> legacy method
-// * Advantages
-// componet life cycle methods
-//  built in state
-
-// * Drawbacks
-// complex
-// slower
-import React from "react";
-class Home extends React.Component {
-  render() {
-    return <h1>Home page</h1>;
-    //   add red border to the div
-  }
+export default function Home() {
+  return <h1>Home</h1>;
 }
-export default Home;
