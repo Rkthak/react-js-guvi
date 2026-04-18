6a5a5b48ff996286d679837ea42b8d5909903385
Rkthak <rkon2609@gmail.com>
2026-04-18 13:52:46 +0530

    compared class and functional components

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+// functional components
+// HOOKS introduce and now support componet life cycle methods & state
+
+function Header() {
+  return (
+    <>
+      <h1>GUVI</h1>
+      <nav>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>practice</li>
+          <li>react</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+// class component ==> legacy method
+// * Advantages
+// componet life cycle methods
+//  built in state
+
+// * Drawbacks
+// complex
+// slower
+import React from "react";
+class Home extends React.Component {
+  render() {
+    return <h1>Home page</h1>;
+    //   add red border to the div
+  }
+}
+export default Home;
