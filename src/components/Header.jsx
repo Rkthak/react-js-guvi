4940c5a4ad5cdef9698825c67c3b1bbd3b2b690b
Rkthak <rkon2609@gmail.com>
2026-04-21 13:36:17 +0530

    help

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import { Component } from "react";
+import Helping from "./Helping";
 
 export default class Header extends Component {
   render() {
     // this.props
     let { name, age } = this.props;
     return (
-      <h1>
-        hello, {name}! You are {age} years old!{" "}
-      </h1>
+      <div>
+        <h1>
+          hello, {name}! You are {age} years old!{" "}
+        </h1>
+        <Helping />
+      </div>
     );
   }
 }
